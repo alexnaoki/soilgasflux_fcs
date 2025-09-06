3fc996ab215cc89b612d2c69fe1df4f6eef55e5a
alexnaoki <alexkobayashi10@gmail.com>
2025-09-06 16:55:37 +0200

    added function to select_bestPareto

--- soilgasflux_fcs/multiprocess_raw_data.py
+++ soilgasflux_fcs/multiprocess_raw_data.py
@@ -7,6 +7,7 @@ import xarray as xr
 import os
 import multiprocessing as mp
 from .fcs import FCS
+from .pareto import Pareto
 
 class Multiprocessor:
     def __init__(self):
@@ -37,7 +38,7 @@ class Multiprocessor:
         a_fcs.settings(moving_window=True, window_walk=10, min_window_size=60, 
                        min_deadband=0, max_deadband=60)
         
-        results = a_fcs.run_MC(n=id, n_MC=1000,metadata={'area':314, 'volume':6283})
+        results = a_fcs.run_MC(n=id, n_MC=8000,metadata={'area':314, 'volume':6283})
         print('Results:', results.keys())
         return results
 
@@ -67,6 +68,21 @@ class Multiprocessor:
             cutoff = list(converted_data[times[0]]['cutoff'])
             deadband = list(converted_data[times[0]]['deadband'])
             print(deadband, cutoff)
+            # print(converted_data)
+            print('test')
+            print(times)
+            print(np.shape(np.array(converted_data[times[0]]['dcdt(HM)'])))
+            # print(np.array([np.array([[0,0],[0,1]]) for t in times]))
+            for t in times:
+                if np.shape(np.array(converted_data[t]['dcdt(HM)'])) != (len(cutoff), len(deadband)):
+                    print('Shape mismatch at time:', t)
+                    print('Expected shape:', (len(cutoff), len(deadband)))
+                    print('Actual shape:', np.shape(np.array(converted_data[t]['dcdt(HM)'])))
+                else:
+                    pass
+            print(np.array([converted_data[t]['dcdt(HM)'] for t in times]))
+
+            print('test0')
 
             ds = xr.Dataset(
                 {
@@ -147,3 +163,63 @@ class Multiprocessor:
             ds.to_netcdf(f'{output_folder}/{chamber_id}_{date}.nc')
             print('NetCDF file saved')
         return ds
+
+    def select_bestPareto(self, ds):
+        '''
+        
+        output: dataset with MCMC results from  the best pareto front
+        '''
+        print('Selecting best pareto front from dataset')
+        deadband = ds.coords['deadband']
+        cutoff = ds.coords['cutoff']
+        n_MC = ds.coords['MC']
+
+
+        time_list = []
+        deadband_list = []
+        cutoff_list = []
+        best_dcdt_list = []
+        for time in ds.coords['time'].values:
+
+            pa = Pareto(dsMC=ds.sel(time=time))
+            Norm_uncertaintyRange, Norm_logprob, flatNorm_uncertaintyRange, flatNorm_logprob = pa.prepare_metrics()
+            pareto_uncertaintyRange_logprob = pa.find_pareto_front(x=flatNorm_uncertaintyRange,
+                                                                            y=flatNorm_logprob, 
+                                                                            maximize_x=False, maximize_y=False)
+            coords_pareto_uncertaintyRange_logprob = pa.get_coords_pareto(pareto_indices=pareto_uncertaintyRange_logprob)
+
+            best_pareto_x, best_pareto_y = pa.get_best_from_pareto(pareto_indices=pareto_uncertaintyRange_logprob, 
+                                                                    metric_x=Norm_uncertaintyRange,
+                                                                    metric_y=Norm_logprob)
+            uncertaintyRange_hm = pa.uncertaintyRange
+            logprob_hm = pa.logprob
+
+            hist_dcdt = ds.sel(time=time, 
+                               deadband=deadband[best_pareto_y].values, 
+                               cutoff=cutoff[best_pareto_x].values)['dcdt(HM)']
+            
+
+            # print('Best pareto point (uncertaintyRange, logprob):', best_pareto_x, best_pareto_y)
+            # print('Deadband:', deadband[best_pareto_y].values)
+            # print('Cutoff:', cutoff[best_pareto_x].values)
+
+            time_list.append(time)
+            deadband_list.append(deadband[best_pareto_y].values)
+            cutoff_list.append(cutoff[best_pareto_x].values)
+            best_dcdt_list.append(hist_dcdt.values)
+
+            
+        print('Best dcdt shape:', np.shape(np.array(best_dcdt_list)))
+
+        ds_best = xr.Dataset(
+            {
+                'dcdt(HM)': (['time', 'MC'], np.array(best_dcdt_list, dtype=np.float32)),
+                'best_deadband': (['time'], np.array(deadband_list, dtype=int)),
+                'best_cutoff': (['time'], np.array(cutoff_list, dtype=int)),
+            },
+            coords={
+                'time': time_list,
+                'MC': n_MC
+            }
+        )
+        return ds_best
